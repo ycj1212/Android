ede43a249f28be5a04eae5c503432c88d4e6b7b1
Yang Cheol Joo <rlaengks0001@naver.com>
2021-10-30 14:54:43 +0900

    Add feature that request permission

--- STTProject/app/src/main/java/com/example/sttproject/MainActivity.kt
+++ STTProject/app/src/main/java/com/example/sttproject/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.sttproject
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+const val STT_PERMISSION_REQUEST_CODE = 101
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.RECORD_AUDIO
+                ), STT_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            STT_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "권한 승인 완료", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "권한 승인 거절", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
